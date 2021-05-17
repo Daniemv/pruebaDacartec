d75f70e65dc9ef32e8b801979192a3660004ccbc
Danimv <dmunozva@everis.com>
2021-05-17 20:54:26 +0200

    created search logic for products

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -9,11 +9,38 @@ import { ProductInterface } from 'src/app/interfaces/product.interface';
 export class ProductComponent implements OnChanges {
 
   @Input() productData: ProductInterface[] = [];
+  originalProductData: ProductInterface[] = [];
 
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges) {
     this.productData = changes.productData.currentValue;
+    this.originalProductData = [...this.productData];
   }
 
+  searchProduct(termToSearch: string): void {
+    if (termToSearch.length > 0) {
+      if (this.productData.length === 0) {
+        this.search(this.originalProductData, termToSearch);
+      }
+      this.search(this.productData, termToSearch);
+    } else if (termToSearch.length === 0) {
+      this.productData = this.originalProductData;
+    } else {
+      this.productData = [];
+    }
+  }
+
+
+  private search(arraySearch: ProductInterface[],termToSearch: string) {
+    const newProductDataCollection: ProductInterface[] = [];
+    arraySearch.forEach(product => {
+      if (product.description.toLowerCase().includes(termToSearch.toLowerCase())) {
+        newProductDataCollection.push(product);
+        this.productData = newProductDataCollection;
+      } else {
+        this.productData = [];
+      }
+    });
+  }
 }
